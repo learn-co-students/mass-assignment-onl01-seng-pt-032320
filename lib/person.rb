d81c2cd17cbdc80c639ab32eda0eceb3488c357e
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-08 00:03:06 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -34,3 +34,17 @@ class Person
   end
 
 end
+
+
+bob_attributes = {name: "Bob", hair_color: "Brown"}
+
+bob = Person.new(bob_attributes)
+bob.name       # => "Bob"
+bob.hair_color # => "Brown"
+
+susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+
+susan = Person.new(susan_attributes)
+susan.name      # => "Susan"
+susan.height    # => "5'11""
+susan.eye_color # => "Green"
